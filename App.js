bc9072f3c86e72ba437538b2a52e909061c69863
Mateus9349 <103339423+Mateus9349@users.noreply.github.com>
2023-02-05 14:32:07 -0400

    testando envio

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet} from 'react-native';
+
+import Home from './src/telas/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <SafeAreaView style={estilos.tela}>
+        <Home />
+        <StatusBar/>
+      </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilos = StyleSheet.create({
+  tela: {
+    flex: 1
   },
-});
+})
